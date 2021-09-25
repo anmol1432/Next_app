67886c631e70c0e91fab85d6a639ba9e28a5e376
unknown <anmolbuttar143205@gmail.com>
2021-09-25 14:03:37 +0530

    fixing css styles inssue for deployemnt styles not working on server

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
 import '../styles/globals.css'
+import '../styles/fotter.module.css'
+import '../styles/Home.module.css'
+import '../styles/midSection.module.css'
+import '../styles/navbar.module.css'
 import 'tailwindcss/tailwind.css'
 import 'antd/dist/antd.css'
+import Router from 'next/router';
+
+
+Router.events.on('routeChangeComplete', () => {
+  if (process.env.NODE_ENV !== 'production') {
+    const els = document.querySelectorAll('link[href*="/_next/static/css/styles.chunk.css"]');
+    const timestamp = new Date().valueOf();
+    els[0].href = '/_next/static/css/styles.chunk.css?v=' + timestamp;
+  }
+})
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
