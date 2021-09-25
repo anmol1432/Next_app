ba18b167a0dd10a4e6b42b8d17597d740c1fe709
unknown <anmolbuttar143205@gmail.com>
2021-09-25 14:50:39 +0530

    fixing css styles inssue for deployemnt styles not working on server

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,31 @@ export default function Home() {
   })
 
   useEffect(() => {
+    Array.from(
+      document.querySelectorAll('head > link[rel="stylesheet"][data-n-p]')
+    ).forEach(node => {
+      node.removeAttribute('data-n-p');
+    });
+    const mutationHandler = mutations => {
+      mutations.forEach(({ target }) => {
+        if (target.nodeName === 'STYLE') {
+          if (target.getAttribute('media') === 'x') {
+            target.removeAttribute('media');
+          }
+        }
+      });
+    };
+    const observer = new MutationObserver(mutationHandler);
+    observer.observe(document.head, {
+      subtree: true,
+      attributeFilter: ['media'],
+    });
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  useEffect(() => {
     form.setFieldsValue({
       Phone: dialCode.dialCode
     })
